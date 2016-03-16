ff0969d9282a11afcd6c9be820bd3bca4f6130ab
Jasmine Walker <jasminewalker@jasmines-mbp.dhcp.nd.edu>
2016-03-15 20:30:43 -0400

    FUCK

--- nrotc.py
+++ nrotc.py
@@ -8,6 +8,37 @@ from jinja2 import Template
 def index():
     return render_template("home.html")
 
+'''
+@app.route("/who-we-are")
+def index():
+    return render_template("home.html")
+
+@app.route("/unit-staff")
+def index():
+    return render_template("home.html")
+
+@app.route("/history")
+def index():
+    return render_template("home.html")
+
+@app.route("/contact")
+def index():
+    return render_template("home.html")
+
+@app.route("/nlw")
+def index():
+    return render_template("home.html")
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+'''
+    
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
