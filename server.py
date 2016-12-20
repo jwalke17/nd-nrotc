1b96c785dfedcedb21336f825db3727b0de9ba2f
Jasmine Walker <jasminewalker@Jasmines-MacBook-Pro.local>
2016-12-19 22:11:03 -0500

    first commit, redoing pretty much everything

---
+++ server.py
@@ -0,0 +1,15 @@
+import SimpleHTTPServer
+import SocketServer
+import signal
+import sys
+
+# https://docs.python.org/2/library/simplehttpserver.html
+
+PORT = 9001
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
