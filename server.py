b6c390ceb56302ae5d1493102430aa638a8f0c53
Jasmine Walker <jasminewalker@Jasmines-MacBook-Pro.local>
2016-12-21 13:17:50 -0500

    redid the server using online code. moving towards navigation abilities

--- server.py
+++ server.py
@@ -1,15 +1,66 @@
-import SimpleHTTPServer
-import SocketServer
-import signal
-import sys
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
 
-# https://docs.python.org/2/library/simplehttpserver.html
+# code copied from http://www.acmesystems.it/python_httpd
 
-PORT = 9001
+PORT_NUMBER = 9001
 
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+	
+	#Handler for the GET requests
+    def do_GET(self):
+        
+        if self.path=="/":
+            self.path="/static/html/main.html"
+        
+        try:
+			#Check the file extension required and
+			#set the right mime type
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+			sendReply = False
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+			if self.path.endswith(".png"):
+				mimetype='image/png'
+				sendReply = True
 
-print "serving at port", PORT
-httpd.serve_forever()
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path) 
+				self.send_response(200)
+				self.send_header('Content-type',mimetype)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+			return
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
