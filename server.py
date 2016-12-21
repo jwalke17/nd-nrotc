02c6be1574d2a4a34b67e1cce997784bd14794cc
Jasmine Walker <jasminewalker@Jasmines-MacBook-Pro.local>
2016-12-21 13:58:34 -0500

    got navigation down. didn't have to use python, which is the point i think

--- server.py
+++ server.py
@@ -1,66 +1,16 @@
-#!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from os import curdir, sep
+import SimpleHTTPServer
+import SocketServer
+import signal
+import sys
 
-# code copied from http://www.acmesystems.it/python_httpd
+# https://docs.python.org/2/library/simplehttpserver.html
 
-PORT_NUMBER = 9001
+PORT = 9001
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-	
-	#Handler for the GET requests
-    def do_GET(self):
-        
-        if self.path=="/":
-            self.path="/static/html/main.html"
-        
-        try:
-			#Check the file extension required and
-			#set the right mime type
 
-			sendReply = False
-			if self.path.endswith(".html"):
-				mimetype='text/html'
-				sendReply = True
-			if self.path.endswith(".jpg"):
-				mimetype='image/jpg'
-				sendReply = True
-			if self.path.endswith(".gif"):
-				mimetype='image/gif'
-				sendReply = True
-			if self.path.endswith(".js"):
-				mimetype='application/javascript'
-				sendReply = True
-			if self.path.endswith(".css"):
-				mimetype='text/css'
-				sendReply = True
-			if self.path.endswith(".png"):
-				mimetype='image/png'
-				sendReply = True
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 
-			if sendReply == True:
-				#Open the static file requested and send it
-				f = open(curdir + sep + self.path) 
-				self.send_response(200)
-				self.send_header('Content-type',mimetype)
-				self.end_headers()
-				self.wfile.write(f.read())
-				f.close()
-			return
-        except IOError:
-            self.send_error(404,'File Not Found: %s' % self.path)
+httpd = SocketServer.TCPServer(("", PORT), Handler)
 
-try:
-	#Create a web server and define the handler to manage the
-	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print 'Started httpserver on port ' , PORT_NUMBER
-	
-	#Wait forever for incoming htto requests
-	server.serve_forever()
-
-except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
-	server.socket.close()
+print "serving at port", PORT
+httpd.serve_forever()
--- static/js/HUD.js
+++ static/js/HUD.js
@@ -6,5 +6,16 @@ $(document).ready(function($){
         if( $(".HUD_dropdown").hasClass("visible") )
             $(".HUD_dropdown").toggleClass("visible");
     });
+	
+	$("nav.HUD_dropdown span").click(function() {
+		var page = "";
+		page = $(this).attr("id");
+		$("main").load("/static/html/" + page + ".html");
+		$("main").click();
+	});
     
 });
+
+window.onload = function() {
+	$('main').load("/static/html/main.html");
+}
