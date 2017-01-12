1a793e348316c63a2ac0133052c83e9091767acf
Jasmine Walker <jasminewalker@Jasmines-MacBook-Pro.local>
2017-01-11 21:09:21 -0500

    restyling

--- static/js/base.js
+++ static/js/base.js
@@ -5,18 +5,24 @@ $(document).ready(function($){
 	$(".HUD_main").load("/static/html/HUD.html");
 	$(".footer_main").load("/static/html/footer.html");
 	
+	
+	// if the nav button is clicked, the dropdown menu is toggled
     $(document).delegate(".HUD_nav_btn", "click", function(){
         $(".HUD_dropdown").toggleClass("visible");
     });
+	
+	// if the user clicks the main document and the dropdown is visible, make it invisible
     $(document).delegate(document, "click", function(e){
         if( !($(e.target).is(".HUD_main *")) && $(".HUD_dropdown").hasClass("visible") )
             $(".HUD_dropdown").toggleClass("visible");
     });
 	
+	// mini dropdown toggled if the user hovers over it
 	$(document).delegate(".HUD_DE", "mouseenter mouseleave", function(e){
 			$(this).children(".HUD_DE_dropdown").toggleClass("visible");
 	});
 	
+	// 
 	$(document).delegate(document, "scroll", function(){
 		var currscroll = $(document).scrollTop();
 		var HUDheight = $(".HUD_main").height();
